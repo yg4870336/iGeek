bdd2b3be6a07ad9c496d4e954c08ba92c11bda00
ZhengYuanchun <luckystar1992@126.com>
2016-03-14 20:08:24 +0800

    see readme version 1.0

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# iGeek
-iGeek team in UCAS
+# iGeek -- (UCAS)
 
 Team Information:
 
-  1.Tandy 
+  1.Tandy(zhengyuanchun)
+
+  emal:luckystar1992@126.com
+
+  2.
+
+version: 1.0
+
+    add the index page( our team information)
---
+++ assets/js/viewCount.js
@@ -0,0 +1,8 @@
+
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?0e61fcdf2ae29b950273162ee0ba26b4";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
